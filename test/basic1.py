e88bb1bdd38e010b5d6143a11754ba99ffe1a233
Brian Sajeev <briansajeev@gmail.com>
2023-10-13 22:12:37 +0530

    Added test files

---
+++ test/basic1.py
@@ -0,0 +1,12 @@
+import socket
+
+UDP_IP = "127.0.0.1"
+UDP_PORT = 9801
+MESSAGE = b"SendSize\n\n"
+
+print("UDP target IP: %s" % UDP_IP)
+print("UDP target port: %s" % UDP_PORT)
+print("message: %s" % MESSAGE)
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Internet  # UDP
+sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
